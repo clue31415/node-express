98ea3111a0895e6239d03b674b2173f16d35056a
clue31415 <clue31415@gmail.com>
2024-10-26 22:18:44 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const nunjucks = require('nunjucks');
+const express = require('express');
+const os = require('os');
+const MongoClient = require('mongodb').MongoClient;
+const MONGODB_URL = process.env.MONGODB_URL || (process.env.NODE_ENV === 'production' ? 'mongodb://mongo:27017/okpogo' : 'mongodb://localhost:27017/okpogo');
+const MONGODB_DBNAME = process.env.MONGODB_DBNAME || 'okpogo';
+const PORT = process.env.PORT || 8080;
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+console.log('MONGODB_URL', MONGODB_URL);
+console.log('MONGODB_DBNAME', MONGODB_DBNAME);
+console.log('PORT', PORT);
+console.log('env', JSON.stringify(process.env, null, 2));
 
-var app = express();
+const app = express()
+  .set('trust proxy', true)
+  .get('/upload', (req, res, next) => {
+    MongoClient.connect(MONGODB_URL, (err, client) => {
+      if (err) return next(err);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'njk');
-nunjucks.configure('views', { 
-  express: app,
-  watch: true,
+      const collection = client.db(MONGODB_DBNAME).collection('post');
+      const userinfo = collection.find({name: req.body.name}).exec();
+      if (userinfo.ban == false && userinfo.pw == req.body.pw){
+        collection.insert([
+          { name: req.body.name },
+          { title: req.body.title },
+          { content: req.body.content }
+        ], { w: 1 }, (err, result) => {
+          if (err) return next(err);
+
+          collection.find({}).toArray((err, data) => {
+            if (err) return next(err);
+
+            /*res.send('<pre>' + JSON.stringify({
+              url: client.s.url,
+              options: client.s.options,
+              poolSize: client.topology.s.poolSize,
+              platform: client.topology.clientInfo,
+              data
+            }, null, 2) + '</pre>');*/
+
+            client.close();
+          });
+        });
+      }
+    });
+  })
+  .get('/read', (req, res) => {
+    MongoClient.connect(MONGODB_URL, (err, client) => {
+      if (err) return next(err);
+
+      const collection = client.db(MONGODB_DBNAME).collection('post');
+      const postinfo = collection.find({});
+
+    res.send(JSON.stringify({postinfo
+    }, null, 2));
+  });
+  });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
