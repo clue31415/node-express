eed210eb7302fa2749bb669be5b091ec70cbcdab
clue31415 <clue31415@gmail.com>
2024-12-17 11:44:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const MONGODB_URL = process.env.MONGODB_URL || (process.env.NODE_ENV === 'produc
 const MONGODB_DBNAME = process.env.MONGODB_DBNAME || 'okpogo';
 const PORT = process.env.PORT || 8080;
 
+const danwordlist = ["씨발", "ㅅㅂ", "ㅆㅂ", "새끼", "지랄", "좆", "엿", "병신", "존나", "썅", "쌍", "죽여", "시발", "돼지", "염병", "정신병자", "한남", "한녀", "년", "페미", "동덕", "게이", "인셀", "걸레", "매춘", "자살", "일베", "승만", "정희", "두환", "명박", "근혜", "무현", "재인", "홍준표", "이재명", "안철수", "조국", "석열", "빨갱", "이기야", "위험단어테스트용용용"];
+
 const app = express();
 app.use(cors({
   origin: 'https://web-react-m2ppprfddd83a9c3.sel4.cloudtype.app',
@@ -23,7 +25,9 @@ app.post('/api/users/upload', async (req, res, next) => {
     const collection = client.db(MONGODB_DBNAME).collection('namepw');
     const userinfo = await collection.findOne({ name: req.body[2] });
     console.log(req.body);
-    if (!userinfo || userinfo.ban || userinfo.pw !== req.body[3] || req.body[0] == null || req.body[1] == null) {
+    const titlecheck = danwordlist.some(char => req.body[0].includes(char));
+    const contentcheck = danwordlist.some(char => req.body[1].includes(char));
+    if (!userinfo || userinfo.ban || userinfo.pw !== req.body[3] || req.body[0] == null || req.body[1] == null || !titlecheck || !contentcheck) {
       console.log('upload not allowed');
       
       return res.status(403).send({ error: 'Forbidden' });
